38623d8d140251d83d6a023b0d0dc1ff8d37abb4
Saif Ali Shaik <saifshine7@gmail.com>
2021-03-09 12:53:05 +0530

    🐛 FIX: state back to pay around

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,32 +4,7 @@ document.onreadystatechange = function () {
   if (document.readyState === 'interactive') renderApp();
 };
 
-function renderApp() {
-  app
-    .initialized()
-    .then((_client) => {
-      window['client'] = _client;
-      appLifeCycleEvent('Initialized');
+/** ~ playground starts ~ */
+function renderApp() {}
 
-      client.events.on('app.activated', () => {
-        appLifeCycleEvent('Activated');
-        client.iparams
-          .get()
-          .then((iparams) => {
-            Object.keys(iparams).forEach(function (key) {
-              notes.insertAdjacentHTML('beforeend', `<li>${key}:${iparams[key]}</li>`);
-            });
-          })
-          .catch(console.error);
-      });
-
-      client.events.on('app.deactivated', () => {
-        appLifeCycleEvent('Initialized');
-      });
-    })
-    .catch(console.error);
-}
-
-function appLifeCycleEvent(state) {
-  return notes.insertAdjacentHTML('beforebegin', `<li>${state}</li>`);
-}
+/** ~play ground ends~ */
