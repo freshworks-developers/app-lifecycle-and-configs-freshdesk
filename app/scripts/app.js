6c02ef3c23cf947fda133149aca7c58e06fd8444
Saif Ali Shaik <saifshine7@gmail.com>
2021-03-09 16:17:58 +0530

    🚀 RELEASE: point: app lifecycle

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,6 +5,26 @@ document.onreadystatechange = function () {
 };
 
 /** ~ playground starts ~ */
-function renderApp() {}
+function renderApp() {
+  app
+    .initialized()
+    .then(function onInit(_client) {
+      window['client'] = _client;
+      appLifeCycleEvent('Initialized');
+
+      client.events.on('app.activated', () => {
+        appLifeCycleEvent('Activated');
+      });
+
+      client.events.on('app.deactivated', () => {
+        appLifeCycleEvent('Deactivated');
+      });
+    })
+    .catch(console.error);
+}
+
+function appLifeCycleEvent(state) {
+  return notes.insertAdjacentHTML('beforebegin', `<li>${state}</li>`);
+}
 
 /** ~play ground ends~ */
